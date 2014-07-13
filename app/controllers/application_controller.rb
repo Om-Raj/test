a58944da24bee9de2b2fd014909a9b10c92f6ffa
pankaj saini <saini.info@gmail.com>
2014-07-13 20:56:27 +0530

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,16 +25,7 @@ class ApplicationController < ActionController::Base
     error_obj[:Exception][:StatusCode] = code
     return error_obj
   end
-
-# creating json success object where need to render nothing
-  def get_success_object(name,msg,code)
-    error_obj = API_SUCCESS
-    error_obj[:Success][:Success] = name
-    error_obj[:Success][:Message] = msg
-    error_obj[:Success][:StatusCode] = code
-    return error_obj
-  end
-
+ 
 
   def zendesk_people_tickets(people_zendesk_id,subdomain_name,access_token,token_type)
     return data_parse_for_tickets(PipeLineDeals.get_all_tickets_people(subdomain_name,people_zendesk_id,access_token,token_type))
@@ -43,8 +34,9 @@ class ApplicationController < ActionController::Base
   def data_parse_for_tickets(json)
     ticket_lists  = []
     unless json.blank?
-      if !json['tickets'].blank? and json['tickets'].length >= 0
-        json['tickets'].each_with_index do |page, index|
+	json_tickets = json['tickets']
+      if json_tickets &&  json_tickets.length >= 0
+        json_tickets.each_with_index do |page, index|
           ticket_lists << { :subject => page['subject'],
                             :description => page['description'],
                             :request_date => DateTime.strptime(page['created_at'],'%Y-%m-%dT%H:%M:%S%z') ,
