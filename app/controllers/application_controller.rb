778479eb1b72dd3c971cb111ca5f42308523274f
pankaj saini <saini.info@gmail.com>
2014-07-16 19:06:54 +0530

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -81,12 +81,17 @@ class ApplicationController < ActionController::Base
   end
 
 
-  def return_json_obj(people,subdomain_name)
+  def return_json_obj(people,subdomain_name,type)
     if people
       code = people[0]
       data = people[1]
       if code == 200
+        if type == "company_or_deals"
         company_or_deals_people_tickets(data,subdomain_name)
+      else
+          people_zendesk_id(data,subdomain_name)
+        end
+
       else
         error_obj = get_exception_object(code,data,code)
         render :json => error_obj.to_json, :status => code  and return
@@ -97,10 +102,33 @@ class ApplicationController < ActionController::Base
   def get_company_or_deals(subdomain_name,pipeline_company_id,pipeline_secret,type)
     if subdomain_name && pipeline_company_id && pipeline_secret
       people = PipeLineDeals.get_people_associated_company_or_deals(pipeline_company_id,pipeline_secret,type)
-      return_json_obj(people,subdomain_name)
+      return_json_obj(people,subdomain_name,"company_or_deals")
     else
+      api_missing_required
+    end
+  end
+
+  def api_missing_required
       error_obj = get_exception_object(I18n.t(:code_400),I18n.t(:api_missing_required),400)
       render :json => error_obj.to_json, :status => 400  and return
+    end
+
+  def people_zendesk_id(people_email,subdomain_name)
+    subdomain_exists = User.subdomain_exists(subdomain_name)
+    unless subdomain_exists.blank?
+      access_token = subdomain_exists[0]['access_token']
+      token_type = subdomain_exists[0]['token_type']
+      people_zendesk_id = PipeLineDeals.get_people_zendesk_id(subdomain_name,people_email,access_token,token_type)
+      response = Hash.new
+      if people_zendesk_id
+
+        response["tickets"] = zendesk_people_tickets(people_zendesk_id,subdomain_name,access_token,token_type)
+        render :json => response.to_json, :status => 200 and return false
+      else
+        error_obj = get_exception_object(302,I18n.t(:people_id_not_found),302)
+        render :json => error_obj.to_json, :status => 302  and return
+      end
+
     end
   end
 
